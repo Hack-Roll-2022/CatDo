610ca9dcc30b64bf489c80d36a8e5687a5759476
Chen-Yiyang <iycspfa@gmail.com>
2022-01-07 11:53:54 +0800

    Implement basic display over other apps

--- app/src/main/java/com/example/catdonotification/MainActivity.java
+++ app/src/main/java/com/example/catdonotification/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.catdonotification;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkOverlayPermission();
+        startService();
+    }
+
+    // method for starting the service
+    public void startService(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // check if the user has already granted
+            // the Draw over other apps permission
+            if(Settings.canDrawOverlays(this)) {
+                // start the service based on the android version
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(new Intent(this, ForegroundService.class));
+                } else {
+                    startService(new Intent(this, ForegroundService.class));
+                }
+            }
+        }else{
+            startService(new Intent(this, ForegroundService.class));
+        }
+    }
+
+    // method to ask user to grant the Overlay permission
+    public void checkOverlayPermission(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                // send user to the device settings
+                Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                startActivity(myIntent);
+            }
+        }
+    }
+
+    // check for permission again when user grants it from
+    // the device settings, and start the service
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startService();
     }
 }
+
